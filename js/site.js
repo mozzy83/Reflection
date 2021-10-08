0a1d223d7d7321ae5d404b3e4881ed5453a92db0
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2021-10-07 18:08:50 -0600

    Completed App, Code, and JS pages

--- js/site.js
+++ js/site.js
@@ -1,4 +1,27 @@
-//Call Hello World
-function helloWorld(){
-    alert("Wello World");
+//Get String from input
+//Controller Function
+function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    if(userString.length < 2){alert("Value must be longer than two characters to reverse")}
+    else{let revString = reverseString(userString);
+    displayString(revString);}
+}
+//Reverse the string
+//Logic Function
+function reverseString(userString) {
+    let revString = [];
+    //reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];        
+    }
+    return revString;
+}
+//Display reversed string to user
+//View Function
+function displayString(revstring) {
+    //write to the page
+    document.getElementById("msg").innerHTML = `Your string's reflection:<br> ${revstring}`;
+    //show alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
