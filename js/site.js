ef387f835002403622ceb9b7bd147affc4aaeef1
mozzy83 <91623379+mozzy83@users.noreply.github.com>
2022-09-24 22:21:32 -0600

    Full build

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-//Call Hello World
-function HellowWorld(){
-    alert("Hello World");
+//Get string from UI
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+function reverseString(userString){
+    let revString = [];
+
+    for (let index = userString.length-1; index >= 0; index--) {
+        revString += userString[index]; 
+    }
+    return revString;
+}
+
+//Display reversed string
+function displayString(revString){
+    document.getElementById("msg").innerHTML = `Your string's reflection is: ${revString}`;
+    document.getElementById("alert").classList.remove("invisible");
 }
